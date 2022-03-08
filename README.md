15ab0f035a748f48e6dfa10b6fbae3256e111c98
kuberlog <collinalexbell@gmail.com>
2022-03-08 13:02:50 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,40 @@
+### work-mode
+
+Keep your work and personal files seperate on the same machine without seperate user
+
+## About the Project
+
+If you want to use the same computer and user for both personal and work purposes, then th
+bout your browser history, `Downloads` folder, or private files being exposed when on a Zo
+
+## How to use
+
+Copy `work-mode.json` to `~/.work-mode.json` or `~/.config/work-mode.json`
+
+Replace default directories in the config file with directories you want to have images for
+
+`work-mode on` when you start work
+
+`work-mode off` when you are finished
+
+### Built with
+
+* `mkfs`
+* `fallocate`
+* `mount`
+
+### How it works
+
+`work-mode` mounts `.img` files on top of existing personal directories, effectively shadowing their contents.
+
+For example, if `Downloads` is in your `.work-mode-dirs`, then a coresponding .img file will be mounted at `Downloads` when `work-mode on` is called, effectively hiding all of your personal downloads. Then when `work-mode off` is called, the img will be unmounted, revealing the personal contents again.
+
+Reccomended folers to shadow:
+* `.config/Firefox` to have seperate broswer histories
+* `Downloads/` so coworkers won't see all the random things you are downloading
+* `pics/` to hide your selfies
+
+### Why not multiple users?
+
+As a developer, there is a lot of configuration and software that should be shared between a work and personal computing environment. Keeping seperate users and user directories for the two environments will lead to duplication of that configuration.
+
---
+++ work-mode.js
@@ -0,0 +1,79 @@
+import crypto from "crypto";
+import util from 'util';
+import child_process from "child_process"
+import path from "path"
+import os from "os";
+import fs from "fs"
+const exec = util.promisify(child_process.exec);
+
+async function dd(of, size) {
+  await exec(`dd if=/dev/zero of=${of} bs=1M count=${size}`);
+}
+
+async function mkfs(shaname) {
+  await exec(`mkfs ext3 -F ${shaname}`)
+}
+
+async function mount(dirName, shaName) {
+  await exec(`sudo mount ${shaName} ${dirName}`)
+}
+
+async function chown(dirName) {
+  await exec(`sudo chown ${os.userInfo().username} ${dirName}`)
+}
+
+async function umount(dirName) {
+  await exec(`sudo umount ${dirName}`)
+}
+
+async function mkdir(dirName) {
+  await exec(`mkdir ${dirName}`)
+}
+
+const config = JSON.parse(fs.readFileSync("work-mode.json"));
+
+function dirToSha(dirname) {
+  const shasum = crypto.createHash("sha256").update(dirname).digest("hex");
+  return shasum.slice(0, 8);
+}
+
+
+const workModeImgDirName  = (config.workModeImgDirName || "~/.work-mode-imgs").replace("~", os.homedir);
+async function workModeOn() {
+  Object.entries(config.directories).forEach(async ([dirname, config]) => {
+    const workModeDirExists = fs.existsSync(workModeImgDirName);
+    if(!workModeDirExists) {
+      mkdir(workModeImgDirName);
+    }
+
+    const shaName = dirToSha(dirname);
+    const fullImgPath = path.format({dir: workModeImgDirName, name: shaName})
+    const imgExists = fs.existsSync(fullImgPath);
+    if(!imgExists) {
+      await dd(fullImgPath, config.size);
+      await mkfs(fullImgPath);
+    }
+    await mount(dirname, fullImgPath);
+    await chown(dirname);
+  })
+}
+
+async function workModeOff() {
+  Object.entries(config.directories).forEach(async ([dirname]) => {
+    await umount(dirname);
+  })
+}
+
+const args = process.argv.slice(2)
+if(args.length > 0){
+  switch(args[0]) {
+    case "on":
+      await workModeOn();
+      break;
+    case "off":
+      await workModeOff();
+      break;
+    default:
+      console.error("usage: work-mode <on/off>")
+  }
+}
